bdc590be66953e10eda12b81aff038d384f5eb9d
Naufal Anbial Falah <75089762+nafnaufal@users.noreply.github.com>
2022-05-19 07:27:58 +0700

    Add files via upload

---
+++ si_sekolah/login/login_action.php
@@ -0,0 +1,21 @@
+<?php
+    $koneksi = mysqli_connect("localhost","root","","sekolah");
+
+    $ni = mysqli_real_escape_string($koneksi,$_POST['no_induk']);
+    $pas = mysqli_real_escape_string($koneksi,$_POST['password']);
+    $sql = "SELECT * FROM login WHERE nomor_induk = $ni";
+    $query = mysqli_query($koneksi,$sql);
+    $data = mysqli_fetch_assoc($query);
+    if(mysqli_num_rows($query)){
+        if(password_verify($pas, $data['password'])){
+            echo "Berhasil Login";
+            header("Refresh:1; url=../guru.php", true, 303);
+        }else{
+            echo "Password Salah";
+            header("Refresh:1; url=./login.html", true, 303);
+        }
+    }else{
+        echo "NO induk tidak ditemukan";
+        header("Refresh:1; url=./login.html", true, 303);
+    }
+?>
