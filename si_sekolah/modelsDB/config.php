f38092192742e078967c4aaacf7a499355d710a3
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-05-30 05:35:23 +0700

    file koneksi

---
+++ si_sekolah/modelsDB/config.php
@@ -0,0 +1,23 @@
+<?php
+    class Koneksi{
+        private static $USERNAME = "root";
+        private static $PASSWORD = "rian";
+        private static $DATABASE = "sekolah";
+        private static $HOSTNAME = "localhost";
+        private static $connection = NULL;
+
+        public static function getConnection(){
+            Koneksi::$connection = mysqli_connect(Koneksi::$HOSTNAME, Koneksi::$USERNAME, Koneksi::$PASSWORD, Koneksi::$DATABASE);
+            return Koneksi::$connection;
+        }
+        public static function destroyConnection(){
+            Koneksi::$connection = NULL;
+        }
+    }
+
+    if(Koneksi::getConnection()){
+        echo "koneksi berhasil";
+    }else{
+        echo "Koneksi gagal";
+    }
+?>
